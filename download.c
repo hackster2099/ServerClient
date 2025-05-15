cff31f71364b3d4b2383b5e88b223511ad3484dd
hackster2099 <faresshiraliEng@gmail.com>
2025-05-15 12:21:26 -0700

    Working code that is capable of downloading all the files

--- download.c
+++ download.c
@@ -2,18 +2,12 @@
 #include <stdlib.h>
 #include <libsocket/libinetsocket.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include <string.h>
 
-# define BUFFER_SIZE 1000
-# define min(a,b) (((a) < (b)) ? (a) : (b))
+#define BUFFER_SIZE 1000
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
-int main(int argc, char * argv[]) {
-
-    // potential software design
-    //char design[] = {'\\', '-', '.', '-', '/'};
-    int count = 0;
-
+int main(int argc, char *argv[]) {
     int fstSize = 10;
     char serverName[BUFFER_SIZE] = ".cs.sierracollege.edu";
     char *serverSelect = (char *)malloc(fstSize * sizeof(char));
@@ -21,133 +15,113 @@ int main(int argc, char * argv[]) {
     char request[10];
 
     printf("Connect to Server --> (newark, london):  ");
-    
-     while(fgets(serverSelect, BUFFER_SIZE, stdin)){
-
-        char *nl = strchr(serverSelect,'\n');
-
-        if(nl){
-
+    while (fgets(serverSelect, fstSize, stdin)) {
+        char *nl = strchr(serverSelect, '\n');
+        if (nl) {
             *nl = '\0';
             break;
-
         }
-        
     }
 
     strcat(serverSelect, serverName);
 
-    // opening a file discriptor
     int fd = create_inet_stream_socket(serverSelect, "3456", LIBSOCKET_BOTH, 0);
-    
-    while(fd < 0){
-
-        printf("\nERROR --> No connection to the port");
+    if (fd < 0) {
+        fprintf(stderr, "\nERROR --> No connection to the port\n");
         exit(1);
     }
 
-    printf("\nConnected to the Socket");
+    printf("\nConnected to the Socket\n");
 
     FILE *s = fdopen(fd, "r+");
+    if (!s) {
+        perror("fdopen failed");
+        close(fd);
+        return 1;
+    }
+
     char response[BUFFER_SIZE];
     char fileName[BUFFER_SIZE];
-    char garbageBuff[3];
-    long size;
     unsigned char buffer[BUFFER_SIZE];
-    long totalRec = 0;
-    long remaining;
-    long bytes_wanted;
-    long bytes_recieved;
+    long size = 0;
 
     printf("\nOption\nL) --> list of files\nD) --> download files\nQ) --> Quit\n");
-    
-    while(fgets(request, 10, stdin)){
 
+    while (fgets(request, sizeof(request), stdin)) {
         char *nl = strchr(request, '\n');
+        if (nl) *nl = '\0';
 
-        if(nl){
-
-            *nl = '\0';
-
-        }
-
-        if(*request == 'Q'){
-
+        if (*request == 'Q') {
             fprintf(s, "QUIT\n");
-            break;    
-
-        }
-
-        else if(*request == 'L'){
-
+            fflush(s);
+            break;
+        } 
+        else if (*request == 'L') {
             fprintf(s, "LIST\n");
-            
-            while(fgets(response, BUFFER_SIZE, s)){
-
-                if(*response == '.'){
-                break;
-
-                }
-
+            fflush(s);
+            while (fgets(response, BUFFER_SIZE, s)) {
+                if (*response == '.')
+                    break;
                 printf("%s", response);
-                fflush(s);
-
             }
+        } 
+        else if (*request == 'D') {
+            printf("\nFile to Download: ");
+            fgets(fileName, BUFFER_SIZE, stdin);
+            nl = strchr(fileName, '\n');
+            if (nl) *nl = '\0';
 
-        }
-
-        else if(*request == 'D'){
-
-           printf("\nFile to Download: ");
-           
-           fgets(fileName, BUFFER_SIZE, stdin);
-
-            char *nl = strchr(fileName, '\n');
-
-            if(nl){
-
-                *nl = '\0';
-
-            }
-
-            FILE *localFile = fopen(fileName, "wb");
-
-            fprintf(s,"SIZE %s\n", fileName);
-
-            fgets(response, BUFFER_SIZE, s);
-
-            sscanf(response, "+OK %ld", &size);
-
-            fprintf(s, "GET %s\n", fileName);
-
-            fread(garbageBuff, 3, 3, s);
-
-            while(totalRec < size){
-
-                remaining = size - totalRec;
-                bytes_wanted = min(remaining, BUFFER_SIZE);
-                bytes_recieved = fread(buffer, 1, bytes_wanted, s);
-                fwrite(buffer,1, bytes_recieved, localFile);
-                totalRec = totalRec + bytes_recieved;
-                printf("End of Loop\n");
-
-                fflush(s);
+            // Request file size
+            fprintf(s, "SIZE %s\n", fileName);
+            fflush(s);
+            if (!fgets(response, BUFFER_SIZE, s)) {
+                fprintf(stderr, "Failed to get response from server.\n");
                 
             }
 
-        }
+            if (sscanf(response, "+OK %ld", &size) != 1 || size <= 0) {
+                printf("Invalid file size or file doesn't exist.\n");
+                
+            }
 
-        else{
+            FILE *localFile = fopen(fileName, "wb");
+            if (!localFile) {
+                printf("File creation failed");
+                
+            }
 
+            fprintf(s, "GET %s\n", fileName);
+            fflush(s);
+
+            // Read and discard the "+OK\n" line
+            if (!fgets(response, BUFFER_SIZE, s)) {
+                printf("Failed to receive +OK for GET.\n");
+                fclose(localFile);
+                continue;
+            }
+
+            long totalRec = 0;
+            while (totalRec < size) {
+                long remaining = size - totalRec;
+                size_t bytesToRead = min(remaining, BUFFER_SIZE);
+                size_t bytesRead = fread(buffer, 1, bytesToRead, s);
+                fwrite(buffer, 1, bytesRead, localFile);
+                totalRec += bytesRead;
+            }
+
+            fclose(localFile);
+
+        } 
+        else {
             printf("\nERROR --> not a valid option");
-            
         }
- 
+
         fflush(s);
         printf("\nOption\nL) --> list of files\nD) --> download files\nQ) --> Quit\n");
+    }
 
-
-    } 
-
-
+    fclose(s);
+    close(fd);
+    free(serverSelect);
+    return 0;
 }
